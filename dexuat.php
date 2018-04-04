81eed72320c2a338f7264d2495608f3277ab5e87
ghhongnhung <36038331+ghhongnhung@users.noreply.github.com>
2018-04-04 19:03:15 +0700

    Update dexuat.php

--- dexuat.php
+++ dexuat.php
@@ -6,32 +6,29 @@
 					          <div class="item  active">
 					            <div class="row">
 					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150">
+					                <a class="thumbnail" href="tocotoco.php"><img style="width: 200px; height: 150px;" alt="" src="https://media.tripnow.vn/res/g68/671340/prof/s460x300/foody-mobile-photo-4-148240694716-473-636347872553525621.jpg">
 					                <div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+	                                                Trà sữa tocotoco</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
                                 	</a>				                
 					              </div>          
 					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150">
+					                <a class="thumbnail" href="KFC.php"><img style="width: 200px; height: 150px;" alt="" src="https://media-cdn.tripadvisor.com/media/photo-s/0f/b2/5f/35/this-is-what-kfc-is-famous.jpg">
 					                <div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+	                                              Nhà hàng KFC </h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                                </a>
 					              </div>
 					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150">
+					                <a class="thumbnail" href="foodcenter.php"><img style="width: 200px; height: 150px;" alt="" src="https://static.hotdeal.vn/images/829/829079/400x400/192461-lau-thai-chua-cay-lau-tom-yum-cho-4-nguoi-ngon-me-ly-tai-food-center.jpg">
 					                <div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+	                                                Foodcenter - Lẩu thái</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                                </a>
 					              </div>        
@@ -40,32 +37,29 @@
 					          <div class="item">
 					            <div class="row">
 					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150">
+					                <a class="thumbnail" href="donuong.php"><img style="width: 200px; height: 150px;" alt="" src="http://toinayangi.vn/wp-content/uploads/2014/09/lau-nuong-gia-re-1.jpg">
 					                <div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+	                                                Đồ nướng Thượng Đình</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                                </a>
 					              </div>          
 					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150">
+					                <a class="thumbnail" href="comnguvi.php"><img style="width: 200px; height: 150px;" alt="" src="https://media.foody.vn/res/g15/149683/s600x600/2017102715527-com-bo.jpg">
 					                <div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+	                                                Cơm ngũ vị</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                            </a>
 	                        	</div>
 					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150">
+					                <a class="thumbnail" href="banhxeo.php"><img style="width: 200px; height: 150px;" alt="" src="http://product.hstatic.net/1000103608/product/bx-kim-cham_large.jpg">
 					                <div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+	                                                Bánh xèo Khương Trung</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                            </a>
 					              </div>        
@@ -74,32 +68,29 @@
 					          <div class="item">
 					            <div class="row">
 					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150">
+					                <a class="thumbnail" href="banhtom.php"><img style="width: 200px; height: 150px;" alt="" src="http://7monngonmoingay.info/wp-content/uploads/2015/03/cach-lam-banh-tom-ho-tay-ha-noi-gion-rum-thom-ngon.jpg">
 					                <div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+	                                                Bánh tôm Tây hồ</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                                </a>
 					              </div>          
 					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150">
+					                <a class="thumbnail" href="taopho.php"><img style="width: 200px; height: 150px;" alt="" src="https://image.vtc.vn/files/f2/2015/06/17/nhung-quan-tao-pho-ngon-nhat-ha-thanh-3.jpg">
 					                <div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+	                                                Vua tào phớ</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                                </a>
 					              </div>
 					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150">
+					                <a class="thumbnail" href="banhcuon.php"><img style="width: 200px; height: 150px;" alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV4Q9xtzSMCn7WZ0MKuRelvRmdfphAnFGKDU3tSMgXP9prgAhZ">
 					                <div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+	                                                Bánh cuốn</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                                </a>
 					              </div>      
