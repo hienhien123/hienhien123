4892cff255497c1f0873c3ac6163ec1a719181d6
ghhongnhung <36038331+ghhongnhung@users.noreply.github.com>
2018-04-04 19:06:51 +0700

    Update tab-review.php

--- tab-review.php
+++ tab-review.php
@@ -1,31 +1,26 @@
 <div class="panel-heading">
                         <ul class="nav nav-tabs">
-                            <li class="active"><a href="#tab1default" data-toggle="tab">Default 1</a></li>
-                            <li><a href="#tab2default" data-toggle="tab">Default 2</a></li>
+                            <li class="active"><a href="#tab1default" data-toggle="tab">Trang 1</a></li>
+                            <li><a href="#tab2default" data-toggle="tab">Trang 2</a></li>
                         </ul>
                 </div>
                 <div class="panel-body">
-                    <div style="margin-left: -30px;" class="tab-content">
+                    <p><h3 style="padding-left: 10px;">Bài review</h3></p>
+                                <hr>
+                    <div style="margin-left: -33px;" class="tab-content">
                         <div class="tab-pane fade in active" id="tab1default">
                         	<ul type="none" class="menu-review list-menu">
-                        		<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>
+                        		<li><a href="php/chaosuon.php"><img style="margin-left: -10px;height: 76px;" src="http://img.tinbaihay.net/ThumbImages/2015/10/02/cach-nau-chao-suon-thom-ngon-ma-don-gian-ngay-tai-nha-96ed_450.jpg" alt="Check">Cháo sườn</a></li>
+								<li><a href="banhtroitau.php"><img style="margin-left: -10px;height: 76px;" src="https://giadinh.tv/wp-content/uploads/2017/07/cach-lam-banh-troi-tau-nhan-dau-xanh.jpg" alt="Check">Bánh trôi tàu</a></li>
+								<li><a href="chuoinepnuong.php"><img style="margin-left: -10px;height: 76px;" src="https://kenh14cdn.com/2017/1-1512239345098.jpg" alt="Check">Chuối nếp nướng</a></li>
+
                         	</ul>
                         </div>
                         <div class="tab-pane fade" id="tab2default">
                         	<ul type="none" class="menu-review list-menu">
-                        		<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>
-								<li><img src="" alt="Check"><a href="#">Check1</a></li>	
+                        		<li><a href="banhducnong.php"><img style="margin-left: -10px;height: 76px;" src="https://images.foody.vn/res/g1/7986/prof/s576x330/foody-mobile-banh-duc1-jpg-131-635850843423052258.jpg" alt="Check">Bánh đúc nóng</a></li>
+                                <li><a href="dingtea.php"><img style="margin-left: -10px;height: 76px;" src="https://media.foody.vn/res/g32/317014/prof/s/foody-mobile-2-jpg-431-636190530585219534.jpg" alt="Check">DingTea</a></li>
+                                <li><a href="banhmyque.php"><img style="margin-left: -10px;height: 76px;" src="http://vivuhanoi.com/wp-content/uploads/2016/04/12806155_1057228387669789_4912764743476733_n.jpg" alt="Check">Bánh mỳ que</a></li>
                         	</ul>
                         </div>
 
