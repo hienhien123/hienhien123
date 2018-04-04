495c892114eac21ce68cfeb21bcfa47e210a5db9
ghhongnhung <36038331+ghhongnhung@users.noreply.github.com>
2018-04-04 19:06:32 +0700

    Update slideshow.php

--- slideshow.php
+++ slideshow.php
@@ -1,6 +1,6 @@
 <!-- Add slideshow -->
 					<div class="slideshow">
-						<div style="margin: 0 -10px; height: 400px;" id="myCarousel" class="carousel slide" data-ride="carousel"> 
+						<div style="margin: 0 -10px 50px -10px; height: 400px;" id="myCarousel" class="carousel slide" data-ride="carousel"> 
 			  
 						  	<ol class="carousel-indicators radio">
 							    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -9,16 +9,16 @@
 						  	</ol>
 
 			  				<div class="carousel-inner">
-			   			 		<div style="width: 100%; height: 400px;" class="item active">
-			      					<img src="https://2.bp.blogspot.com/-3_ZD_L078Hs/Vxb-hachD6I/AAAAAAAAhd8/AhD5Up29_MMeavhbxT567_UMfjA0mDavwCLcB/s1600/kfc-9.png" alt="KFC">
+			   			 		<div  class="item active"><a href="https://www.foody.vn/ha-noi/khuyen-mai">
+			      					<img style="width: 100%; height: 400px;" src="https://giamgiaz.com/wp-content/uploads/2016/07/khuye%CC%82%CC%81n-ma%CC%83i-dealtoday-tha%CC%81ng-7-a%CC%86n-uo%CC%82%CC%81ng.png" alt="HaiSan"></a>
 			    				</div>
 
-			    				<div style="width: 100%; height: 400px;" class="item">
-			      					<img src="https://2.bp.blogspot.com/-3_ZD_L078Hs/Vxb-hachD6I/AAAAAAAAhd8/AhD5Up29_MMeavhbxT567_UMfjA0mDavwCLcB/s1600/kfc-9.png alt="Tocotoco">
+			    				<div class="item"><a href="https://www.foody.vn/ha-noi/khuyen-mai">
+			      					<img style="width: 100%; height: 400px;" src="https://www.khuyenmaivui.com/wp-content/uploads/2016/05/Pizza-Hut-khuyen-mai-set-an-trua-tu-49k-set-an-toi-tu-69k-thu-2-den-thu-6.jpg" alt="PizzaHut"></a>
 			    				</div>
 
-			    				<div style="width: 100%; height: 400px;" class="item">
-			      					<img src="https://2.bp.blogspot.com/-3_ZD_L078Hs/Vxb-hachD6I/AAAAAAAAhd8/AhD5Up29_MMeavhbxT567_UMfjA0mDavwCLcB/s1600/kfc-9.png alt="Foodcenter">
+			    				<div class="item"><a href="https://www.foody.vn/ha-noi/khuyen-mai">
+			      					<img style="width: 100%; height: 400px;" src="http://media.thethaovanhoa.vn/2016/12/21/23/35/Toss1.jpg" alt="BanhMyChao"></a>
 			    				</div>
 			  				</div>
 
