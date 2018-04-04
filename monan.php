c0f56cef7089da16d7be4c73f874a965c4cab86f
ghhongnhung <36038331+ghhongnhung@users.noreply.github.com>
2018-04-04 19:04:43 +0700

    Update monan.php

--- monan.php
+++ monan.php
@@ -1,57 +1,51 @@
-<div class="MonAn">
+<div id="hapdan" class="MonAn">
 						<p><h4 style="width: 20%; margin-left: 15px; padding-left: 10px; border-bottom: 3px solid orange;">Món ăn hấp dẫn</h4></p>
 					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
+					                <a class="thumbnail" href="goicuon.php"><img style="width: 220px; height: 150px; text-decoration: none;" alt="" src="http://toinayangi.vn/wp-content/uploads/2015/06/m%C3%B3n-%C4%83n-vi%E1%BB%87t-nam-n%E1%BB%95i-ti%E1%BA%BFng-g%E1%BB%8Fi-cu%E1%BB%91n.jpg"><div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+													Gỏi cuốn</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                            </a>
 	                            </div>
 	                            <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
+					                <a class="thumbnail" href="gimbap.php"><img style="width: 220px; height: 150px; text-decoration: none;" alt="" src="http://dulichhanquoc.travel/wp-content/uploads/2014/05/cac-mon-an-han-quoc-kimbap.jpg"><div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+													Cơm cuộn Gimbap</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                            </a>
 	                            </div>
 	                            <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
+					                <a class="thumbnail" href="comchayninhbinh.php"><img style="width: 220px; height: 150px; text-decoration: none;" alt="" src="http://2.bp.blogspot.com/-Dfzo8GYauzg/VFsvKJXUQcI/AAAAAAAAHfI/Sb3hY4BJaZU/s1600/comchay.jpg"><div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+													Cơm cháy Ninh Bình</h5>
+	                                           <?php include 'show-rate.php';?>
 	                                </div>
 	                            </a>
 	                            </div>
 	                        </div>
 					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
+					                <a class="thumbnail" href="myquang.php"><img style="width: 220px; height: 150px; text-decoration: none;" alt="" src="http://monngonmoingay.com/wp-content/uploads/2015/08/201506251048520000001_monngonmoingay.com-miquang-1.png"><div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+												Mỳ Quảng</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                            </a>
 	                            </div>
 	                            <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
+					                <a class="thumbnail" href="taide.php"><img style="width: 220px; height: 150px; text-decoration: none;" alt="" src="http://hoanggiang.com.vn/wp-content/uploads/2017/07/de-tai-ninh-binh2.jpg"><div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+												Tái dê Ninh Bình</h5>
+	                                           <?php include 'show-rate.php';?>
 	                                </div>
 	                            </a>
 	                            </div>
 	                            <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
+					                <a class="thumbnail" href="comamphu.php"><img style="width: 220px; height: 150px; text-decoration: none;" alt="" src="https://cdn3.ivivu.com/2014/11/33-mon-ngon-viet-nam-ivivu16.jpg"><div class="info">
 	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
+												Cơm âm Phủ Huế</h5>
+	                                            <?php include 'show-rate.php';?>
 	                                </div>
 	                            </a>
 	                            </div>
