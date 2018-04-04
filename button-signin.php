c8f0878fdbd91144e8173e382186c3d1fd8834e0
manhchung <36047488+manhchung@users.noreply.github.com>
2018-04-04 18:46:50 +0700

    Update button-signin.php

--- button-signin.php
+++ button-signin.php
@@ -3,13 +3,16 @@
 			<a href="login.php?page=dangnhap" class="btn btn-success">Đăng nhập</a>
 			<a href="register.php?page=dangki" class="btn btn-success">Đăng kí</a>
 			
+			
 			<?php if(isset($_SESSION["loged"])){
-				
+				if($_SESSION["admin"] == '4697'){
+					echo "<a href='dang-review.php' class='btn btn-primary'>Đăng Review</a>";
+				}
+				echo $_SESSION['fullname'];
 				echo "<a href='index.php?act=logout' class='btn btn-danger'>Đăng xuất</a>";
 			?>
 
-				<script type="text/javascript">$('.btn-success').hide();</script>
-						<?php
-			 } ?>
+				<script type="text/javascript" src="BTL_js/hide.js"></script>
+						<?php } ?>
 		</div>
 		</div>
