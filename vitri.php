f7a1db87c6378e2072f4ac2b130c93cacf75257c
manhchung <36047488+manhchung@users.noreply.github.com>
2018-04-04 18:53:59 +0700

    Update vitri.php

--- vitri.php
+++ vitri.php
@@ -1,6 +1,6 @@
 <div class="col-md-8"><!-- Vị trí -->
           <ul style="padding-left: 100px;" class="nav nav-tabs">
-              <li role="presentation"><a href="#">Liên hệ</a></li>
-              <li role="presentation"><a href="#">Khuyến mãi</a></li>
+              <li role="presentation"><a href="#contact">Liên hệ</a></li>
+              <li role="presentation"><a href="https://www.foody.vn/ha-noi/khuyen-mai">Khuyến mãi</a></li>
             </ul>
         </div>
