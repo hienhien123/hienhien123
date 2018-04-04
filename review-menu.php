20977816add0320d5789c0e09be9341559b8ad9c
ghhongnhung <36038331+ghhongnhung@users.noreply.github.com>
2018-04-04 19:06:07 +0700

    Update review-menu.php

--- review-menu.php
+++ review-menu.php
@@ -1,58 +1,42 @@
-				<div class="review">
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	
+</head>
+<body>
+
+			<div class="review">
 						<p><h4 style="width: 20%; margin-left: 15px;padding-left: 10px; border-bottom: 3px solid orange;">Review mới</h4></p>
-					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
-	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
-	                                </div>
-	                            </a>
+										<?php 
+												$connect = mysqli_connect('localhost','root','','db');
+												mysqli_set_charset($connect,"utf8");
+
+												$rows = mysqli_query($connect,"
+												select * from review
+												");
+												while($info=mysqli_fetch_assoc($rows)){
+												$name = $info['name'];
+												$address = $info['address'];
+												$image = $info['image'];
+												$href =$info['href'];
+												?>
+
+											
+							                       
+							                        	<ul type="none" class="col-md-4 list-review">
+							                        		
+								                        <li style="float: left;"><?php echo "<a style='height: 260px;text-decoration:none;' class='thumbnail' href='".$href."'>" ?>
+								                        		<?php echo "<img style='width: 220px; height: 250px;' alt='' src='".$image." '>" ?>
+								                        					<h4 style="margin-top: -100px; color: #ffffff;"> <?php echo "$name" ?>
+								                        					<br>Địa chỉ: <?php echo "$address" ?>	</h4>						
+								                        			</a>
+								                        	</li>
+							                        	</ul>
+
+							            <?php } ?>
+									
 	                            </div>
-	                            <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
-	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
-	                                </div>
-	                            </a>
-	                            </div>
-	                            <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
-	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
-	                                </div>
-	                            </a>
-	                            </div>
-	                        
-					              <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
-	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
-	                                </div>
-	                            </a>
-	                            </div>
-	                            <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
-	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
-	                                </div>
-	                            </a>
-	                            </div>
-	                            <div class="col-md-4">
-					                <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"><div class="info">
-	                                            <h5>
-	                                                Sample Product</h5>
-	                                            <i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star">
-	                                            </i><i class="price-text-color fa fa-star"></i><i class="price-text-color fa fa-star"></i><i class="fa fa-star"></i>
-	                                </div>
-	                            </a>
-	                            </div>
-	                </div>
+	                            
+	</body>
+</html>	
